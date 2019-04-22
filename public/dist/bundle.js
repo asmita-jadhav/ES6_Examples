f7ea3b7e57bdd8ec8884e4c21e2605ffa56c6824
asmita-jadhav <jasmita93@gmail.com>
2019-04-22 18:51:02 +0530

    Add files via upload

---
+++ public/dist/bundle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _Todo = require('./scripts/Todo');
+
+var _Todo2 = _interopRequireDefault(_Todo);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var todo = new _Todo2.default();
